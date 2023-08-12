2b997db17ad0cea0e52789a76494092f9f42a4f7
Software Engineer <marpipe@marpipe.myfiosgateway.com>
2023-08-12 01:03:01 -0400

    build a controller and routes for SupportTicket

---
+++ app/controllers/support_tickets_controller.rb
@@ -0,0 +1,40 @@
+class SupportTicketsController < ApplicationController
+  before_action :set_support_ticket, only: [:update]
+
+  # GET /support_tickets 
+  def index
+    @support_tickets = SupportTicket.all
+    render json: @support_tickets
+  end
+
+  # POST /support_tickets 
+  def create
+    @support_ticket = SupportTicket.new(support_ticket_params)
+
+    if @support_ticket.save
+      render json: @support, status: :created
+    else
+      render json: @support.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /support_tickets/1 
+  def update
+    if @support_ticket.update(support_ticket_params)
+      render json: @support_ticket
+    else
+      render json: @support_ticket.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_support_ticket
+      @support_ticket = SupportTicket.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def support_ticket_params
+      params.require(:support_ticket).permit(:name, :email, :description, :status)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      root "support_ticket#create"
+      resources :support_tickets, only: [:index, :create, :update]
+    end
+  end
 end
